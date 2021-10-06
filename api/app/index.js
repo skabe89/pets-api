fcae995d737f6dde67b194389d9e749fd0ef1dda
Sean Abe <skabe89@gmail.com>
2021-10-05 21:42:50 -0400

    api folder

--- api/app/index.js
+++ api/app/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
 
 app.listen(port, () => {
     console.log(`Listening on localhost:${port}`)

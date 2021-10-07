c17a59f0392a4d9511bc515d6e7e4473fa4e81a3
Sean Abe <skabe89@gmail.com>
2021-10-06 20:49:33 -0400

    added pets array

--- api/app/index.js
+++ api/app/index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3001
+let pets = [
+    {name: "Peppers",age: 10,type: "Dog",color: "White/Black"},
+    {name: "Shaggy",age: 12,type: "Cat",color: "White"},
+
+]
 
 app.get('/', (req, res) => {
     res.send('Hello World')
+})
+
+app.get('/pets', (req, res) => {
+    res.send(pets)
 })
 
 app.listen(port, () => {
